5c43559397629ee5b8fb15232af1e341b3b16257
deltea <thcheetah777@gmail.com>
2026-02-21 14:19:28 -0800

    add the background video

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { fade, slide } from "svelte/transition";
+
+  let backgroundVideo: HTMLVideoElement;
+
+  var isStarted = $state(false);
+
+  function start() {
+    isStarted = true;
+    backgroundVideo.play();
+  }
+</script>
+
+<main class="h-screen w-screen relative">
+  {#if !isStarted}
+    <div class="fixed bg-bg size-full flex opacity-80 justify-center items-center z-10" transition:fade>
+      <button onclick={start} class=" bg-fg text-bg px-4 py-3 cursor-pointer rounded">ENTER</button>
+    </div>
+  {/if}
+
+  <!-- background -->
+  <!-- svelte-ignore a11y_media_has_caption -->
+  <video
+    bind:this={backgroundVideo}
+    src="/background.mp4"
+    class="fixed h-full object-cover object-left -z-10"
+  ></video>
+</main>
