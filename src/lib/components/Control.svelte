6db2293e7d06d9718906e72f7a21e1b1a327e296
deltea <thcheetah777@gmail.com>
2026-02-22 12:27:07 -0800

    make the text shadows better

--- src/lib/components/Control.svelte
+++ src/lib/components/Control.svelte
@@ -8,8 +8,10 @@
 </script>
 
 <div class="flex gap-0.5 items-center text-3xl">
-  <span class="rounded-full size-8 font-sans font-bold bg-bg-dark/80 text-xl border-3 shadow-border border-fg flex items-center justify-center">
+  <span class="rounded-full size-8 font-sans font-bold bg-bg-dark/80 outline-3 shadow-under outline-border text-xl border-3 border-fg flex items-center justify-center">
     {key}
   </span>
-  <span class="text-shadow-border">{@render children()}</span>
+  <span class="text-shadow-border font-sans tracking-tight italic">
+    {@render children()}
+  </span>
 </div>
